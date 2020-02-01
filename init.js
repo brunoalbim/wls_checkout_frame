9bab458e987585a52d36404dd5d09441de18744b
brunoalbim <bruno_albim@hotmail.com>
2020-02-01 17:34:16 -0300

    Update init.js

--- init.js
+++ init.js
@@ -1,3 +1,11 @@
+$("body").append(
+'<div class="wls-grid-a5f48f1g23sas9">'+
+  '<div class="wls-fundomodal-ads80d8as09d8s"></div>'+
+  '<svg class="wls-buttonclose-da6fd53g8adfss" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>'+
+  '<iframe class="wls-frame-as76dasf76d7as86d" frameborder="0" src="https://app.workleads.com.br/checkout/'+ urlID +'"></iframe>'+
+'</div>'
+);
+
 function wls_loadcheckout(urlID, config) {
     if (config.full === true) {
       $(".wls-frame-as76dasf76d7as86d").addClass("wls-fullmodal-as76dasf76d7as86d");
